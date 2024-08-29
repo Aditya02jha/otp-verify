834500b58517715def4ed9667fafc7c01c369c9b
aditya <aditya0000jha@gmail.com>
2024-08-29 19:43:58 +0530

    frontend done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Components/Home'
-import Otp from './Components/Otp'
-import "./index.css"
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Otp from "./Components/Otp";
+import "./index.css";
+import { RecoilRoot } from "recoil";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/otp' element={<Otp />} />
-        </Routes>
-      </BrowserRouter>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/otp" element={<Otp />} />
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,24 +1,37 @@
 import React , {useState}from 'react'
 import { Navigate, useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { phone } from '../atoms/phone';
+import toast ,  { Toaster } from 'react-hot-toast';
 
 const Home = () => {
   const Navigate = useNavigate();
-    const [ph, setph] = useState(91);
+    const [ph, setph] = useRecoilState(phone);
     function handleSubmit(event){
         event.preventDefault();
         console.log(ph);
-        Navigate('/otp');
-
+        if(ph.length == 10){
+          toast.success("OTP Sent Succesfully!");
+          setTimeout(() => {
+            Navigate('/otp');
+          }, 500);
+          
+        }
+        else{
+          toast.error("Please Enter Valid Phone Number");
+        }
     }
   return (
     <div className='flex justify-center bg-black items-center h-screen w-screen text-white rounded-sm '>
+    <Toaster/>
         <form onSubmit={handleSubmit}>
             <input 
-            placeholder="Enter UserName" 
+            placeholder="Enter phone Number" 
             onChange={(event)=>{setph(event.target.value)}} 
             value ={ph} 
             type="number" 
             className='p-5 rounded-sm m-5 text-black'
+            minLength={10}
             />    
             <button className='bg-green-400' type='submit'>GENERATE OTP</button>       
         </form>
